84f4f334dd6c263da00d9c86845dff2f6dbfc7c5
vumg <ab17100222@gmail.com>
2020-02-22 16:01:04 -0600

    main

---
+++ js/main.js
@@ -0,0 +1,64 @@
+const canvas = document.getElementById("canvas");
+
+let _w = window.innerWidth;
+let _h = window.innerHeight;
+
+const renderer = new PIXI.Renderer({
+  view: canvas,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  backgroundColor: 0x0498dc,
+  resolution: window.devicePixelRatio,
+  autoDensity: true
+});
+
+const resize = () => {
+  _w = window.innerWidth;
+  _h = window.innerHeight;
+  renderer.resize(_w, _h);
+};
+
+window.addEventListener("resize", resize);
+
+const stage = new PIXI.Container();
+
+const loader = PIXI.Loader.shared;
+
+loader.add("cloud", "/assets/Clouds_1.png");
+
+// Css style for icons
+const defaultIcon = "url('assets/ufo.png'),auto";
+// Add custom cursor styles
+renderer.plugins.interaction.cursorStyles.default = defaultIcon;
+
+loader.load((loader, resources) => {
+
+  const ticker = new PIXI.Ticker();
+
+  for (let i = 0; i < 100; i++) {
+    const texture = PIXI.Texture.from("./assets/Clouds_1.png");
+    const cloud = new PIXI.Sprite(texture);
+    cloud.x = Math.random() * renderer.screen.width;
+    cloud.y = Math.random() * renderer.screen.height;
+    cloud.scale.x = 1.8;
+    cloud.scale.y = 1.8;
+    cloud.anchor.x = 0.5;
+    cloud.anchor.y = 0.5;
+    stage.addChild(cloud);
+
+    ticker.add(() => {
+      cloud.x -= 0.3;
+      if (cloud.x <= -80) {
+        cloud.x = renderer.width + 100;
+        cloud.y = Math.random() * renderer.height;
+      }
+    });
+  }
+
+  const animate = () => {
+    renderer.render(stage);
+  };
+
+  ticker.add(animate);
+  ticker.start();
+});
